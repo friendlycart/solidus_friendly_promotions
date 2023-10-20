e012b9746c0c68b75b514d4cb6de8073afeef4f9
Martin Meyerhoff <mamhoff@gmail.com>
2023-10-20 16:20:57 +0200

    Friendly Promotion Discounter: Collect eligibility results
    This also includes a `sucess?` and `errors_for` method on eligibility
    results.

--- app/models/solidus_friendly_promotions/eligibility_results.rb
+++ app/models/solidus_friendly_promotions/eligibility_results.rb
@@ -21,5 +21,31 @@ module SolidusFriendlyPromotions
     def for(promotion)
       results_by_promotion[promotion]
     end
+
+    def success?(promotion)
+      results_for_promotion = self.for(promotion)
+      return true unless results_for_promotion
+      promotion.actions.any? do |action|
+        action.relevant_rules.all? do |rule|
+          results_for_promotion[rule].present? &&
+            results_for_promotion[rule].any?(&:success)
+        end
+      end
+    end
+
+    def errors_for(promotion)
+      results_for_promotion = self.for(promotion)
+      return [] unless results_for_promotion
+      results_for_promotion.map do |rule, results|
+        next if results.any?(&:success)
+        results.detect { |r| !r.success }&.message
+      end.compact
+    end
+
+    private
+
+    def item_results_eligible?(klass, resultset)
+      resultset[klass].nil? || resultset[klass].any? { |result| result.success }
+    end
   end
 end
